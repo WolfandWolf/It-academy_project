0ba0f9309b17afeb0b99836397e7e5fb377ab65b
Anton Rasoha <antonos374@gmail.com>
2019-08-02 09:43:29 +0300

    open-close_language

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
+
+
+
 function open_block () {
-    var h = document.getElementById('hide-container');
-    h.style.display = 'block';
+  var h = document.getElementById('hide-container');
+  
+    if (document.getElementById('hide-container').style.display == 'none') {
+       h.style.display = 'block';
+    }
+    else {
+      h.style.display = 'none';
+    }
   }
